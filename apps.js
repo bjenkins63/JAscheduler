9cf72c65b97d760be8cf8c860ab11d2d73facca3
Brian Jenkins <studiojenkins@yahoo.com>
2021-01-25 14:35:26 -0500

    revised layout, still function issues

--- apps.js
+++ apps.js
@@ -7,81 +7,60 @@ $(document).ready(function () {
     const hourOfDay = parseInt((dayjs().format("H")));
     let indexCounter = 0;
 
-    //console.log(typeof hourOfDay);
-
    const militaryTime = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+   $("#mil8").attr("data-hour", militaryTime[0]);
+   $("#mil9").attr("data-hour", militaryTime[1]);
+   $("#mil10").attr("data-hour", militaryTime[2]);
+   $("#mil11").attr("data-hour", militaryTime[3]);
+   $("#mil12").attr("data-hour", militaryTime[4]);
+   $("#mil13").attr("data-hour", militaryTime[5]);
+   $("#mil14").attr("data-hour", militaryTime[6]);
+   $("#mil15").attr("data-hour", militaryTime[7]);
+   $("#mil16").attr("data-hour", militaryTime[8]);
+
+
+
+    //have to get the hour of meeting selected and compare to the data-hour att
+    //if it's past tense add attr of hourPast
+
+    
+   var MeetingName = document.querySelector("meetingCopy");
+   var saveMeeting = document.querySelector("")
+
 
     // Return the current day, month and day
     $("#currentDay").text(dayOfWeek + ", " + currentMonth + " " + dayOfMonth);
-    //$("#currentDay").css("color", "red");
+
+
+    MeetingSaveButton.addEventListener("click", function(event){
+        event.preventDefault();
+        })
 
     $(".meetingSave").on("click", function () {
-        //	const paragraph = document.querySelector("MeetingSave");
-        //	paragraph.textContent = "innerText"
+        localStorage.setItem("meetingCopy", "innerText")
+        append innerText with meetingCopy
 
-        //console.log ("click me");
+        appendClass(".filled");
+
 
     });
 
-    $("#mil8").attr("data-hour", militaryTime[0]);
-    $("#mil9").attr("data-hour", militaryTime[1]);
-    $("#mil10").attr("data-hour", militaryTime[2]);
-    $("#mil11").attr("data-hour", militaryTime[3]);
-    $("#mil12").attr("data-hour", militaryTime[4]);
-    $("#mil13").attr("data-hour", militaryTime[5]);
-    $("#mil14").attr("data-hour", militaryTime[6]);
-    $("#mil15").attr("data-hour", militaryTime[7]);
-    $("#mil16").attr("data-hour", militaryTime[8]);
 
+     var meeting = {
+        meetingSave: MeetingSave.input()
+     }
 
+    function addClass() { 
+        var v = document.getElementById("e"); 
+        v.className += "filled"; 
+        } 
 
-    //add data attribute to label
-    // Array of hours in day 9-17
-    // Looping through the array of movies
-    //for (var i = 0; i < militaryTime.length; i++) {
+    var mtg = document.getElementById("meetingSave").value;
+        localStorage.setItem("meeting", mtg);
 
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-
-
-     //  var a = $(".labelTimeDay").attr("data-hour", militaryTime[indexCounter]);
-
-      // indexCounter++;
-    
-        //// Adding a class of movie-btn to our button        // Adding a data-attribute
-        //a.attr("data-hour", militaryTime[i]);
-
-
-        //console.log(militaryTime[i]);
-
-        // Providing the initial button text
-       // a.text(movies[i]);
-        // Adding the button to the buttons-view div
-       // $("#buttons-view").append(a);
-     // }
-    
-
-
-
-    // MeetingSaveButton.addEventListener("click", function(event){
-    //     event.preventDefault();
-    // })
-
-    // var meeting = {
-    //     meetingSave: MeetingSave.input()
-    // }
-
-    // function addClass() { 
-    //     var v = document.getElementById("e"); 
-    //     v.className += "filled"; 
-    // } 
-
-    // var mtg = document.getElementById("meetingSave").value;
-    //     localStorage.setItem("meeting", mtg);
-
-    // var setMeeting = localStorage.getItem("MeetingSave");
-    //append div add text to time block
-
+        var setMeeting = localStorage.getItem("MeetingSave");
+        ppend div add text to time block
 
 });
 
