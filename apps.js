57da244ca8c7adfdc133a927b47da934221600f8
Brian Jenkins <studiojenkins@yahoo.com>
2021-01-28 14:59:16 -0500

    revised functionality

--- apps.js
+++ apps.js
@@ -11,35 +11,88 @@ $(document).ready(function () {
     $("#currentDay").text(`${dayOfWeek}, ${currentMonth} ${dayOfMonth}, ${hourOfDay}:${minuteOfDay}`);
 
 
-    if (hourOfDay > 10) {
-        $("#mil9").addClass("past");
-      };
+
+    //need to give these divs a numeric value
+
+    function adjHour() {
+
+        $("#mil9").val(9);
+        $("#mil10").val(10);
+        $("#mil11").val(11);
+        $("#mil12").val(12);
+        $("#mil13").val(13);
+        $("#mil14").val(14);
+        $("#mil15").val(15);
+        $("#mil16").val(16);
+        return adjHour;
+    }
+
+    // GIVE DIVS THEIR CORRECT TIME VALUE ON LOAD
+
+    //compare the div numeric value to the current time (hourOfDay)
+    //if value is = do nothing; if > add .future class; if < add .past class  
+
+    if (adjHour > hourOfDay); {
+        $.addClass("past");
+    };
+
+    if (adjHour < hourOfDay); {
+        $.addClass("future");
+    };
+
+    if (adjHour === hourOfDay); {
+        return;
+    }
+}
 
 
 
-    //   var mil9 = 9;
-      
+//want the btn in the hour div to change class of div to (.reserved)when clicked, 
+//and deactivate the text field and button
+//Save Meeting Details (meetingCopy.put.innerText)
+//Get meetingCopy from localstorage and center in the ().reserved) hour div
+
+    $("btn").click(function () {
+    $("#").addClass("reserved");
+});
 
 
-    //   const militaryTime = [ 9, 10, 11, 12, 13, 14, 15, 16];
 
-    //   $("#mil9").attr("data-hour", militaryTime[0]);
-    //   $("#mil10").attr("data-hour", militaryTime[1]);
-    //   $("#mil11").attr("data-hour", militaryTime[2]);
-    //   $("#mil12").attr("data-hour", militaryTime[3]);
-    //   $("#mil13").attr("data-hour", militaryTime[4]);
-    //   $("#mil14").attr("data-hour", militaryTime[5]);
-    //   $("#mil15").attr("data-hour", militaryTime[6]);
-    //   $("#mil16").attr("data-hour", militaryTime[7]);
+function setMeeting() {
+    let meetingCopy = localStorage.getItem("meetingCopy");
+    setMeeting();
+}
 
-    //   document.getElementById("mil9").innerHTML = "Good day!";
+let meeting = document.querySelector("meetingCopy").nodeValue;
+localStorage.setItem("meetingCopy", meeting);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const newLocal = newFunction();
+
+    //var MilitaryTime = [9, 10, 11, 12, 13, 14, 15, 16];
+
+
 
 
 //     $(document).ready(function() {
 //         $('#st').change(function(){
 //             var st = $('#st9:00').val(); // start time Format: '9:00 PM'
 //             var et = $('#et10:00').val(); // end time   Format: '11:00 AM' 
-    
+
 //             //how do i compare time
 //                 if(st > et)
 //                 {
@@ -59,14 +112,6 @@ $(document).ready(function () {
 
 
 
-//     function setMeeting() {
-//         let meetingCopy = localStorage.getItem("meetingCopy");
-//         setMeeting();
-//     }
-
-//     let meeting = document.querySelector("meetingCopy").nodeValue;
-//     localStorage.setItem("meetingCopy", meeting);
-
 
 
 
@@ -77,4 +122,6 @@ $(document).ready(function () {
 
 
 });
+
+
 
